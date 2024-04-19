7bcb00426b9a1721b40e932cd9469efa8b26cc4b
Yoppy Yunhasnawa <yunhasnawa@ymail.com>
2024-04-19 09:32:13 +0700

    Make deployment script into single line

--- deployment.sh
+++ deployment.sh
@@ -1,8 +1,1 @@
-  gcloud functions deploy pymacscannerEndpoint \
-    --gen2 \
-    --runtime=php82 \
-    --region=asia-southeast2 \
-    --source=. \
-    --entry-point=pymacscannerEndpoint \
-    --trigger-http \
-    --allow-unauthenticated
+  gcloud functions deploy pymacscannerEndpoint --gen2 --runtime=php82 --region=asia-southeast2 --source=. --entry-point=pymacscannerEndpoint --trigger-http --allow-unauthenticated
